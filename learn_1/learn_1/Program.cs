6a82e98f6fb5fae2e24c815c51147dd8d2bb326b
Андрей Бодю <152972568+AndreyBodu@users.noreply.github.com>
2023-12-26 13:33:45 +0300

    Learn_1

---
+++ learn_1/learn_1/Program.cs
@@ -0,0 +1,24 @@
+﻿StreamReader str = new StreamReader("task1.txt");
+string Text = str.ReadToEnd();
+int count = 0;
+int max_count = 0;
+for (int i = 0; i < Text.Length; i++)
+{
+    if (Text[i] == 'A' || Text[i] == 'O' || Text[i] == 'E' || Text[i] == 'U' || Text[i] == 'Y' || Text[i] == 'I')
+    {
+        if (max_count < count)
+        {
+            max_count = count;
+        }
+        
+        count=0;
+    }
+    else
+    {
+        count++;
+    }
+
+
+}
+Console.WriteLine(max_count);
+Console.WriteLine(str.ReadToEnd());
